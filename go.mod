7218b2f2c2c67ecd18482d10e5a502acf606a519
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-10-10 23:33:13 +0600

    csv upgrade

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module BulkCertificateACM
 go 1.17
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
--- make.go
+++ make.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"github.com/atotto/clipboard"
 	"github.com/fogleman/gg"
 	"image"
 	"image/color"
@@ -9,6 +12,13 @@ import (
 )
 
 func main() {
+	changed := clipboard.WriteAll("gsdwee")
+	println(changed)
+
+	//readData()
+}
+
+func main_fuc() {
 	const S = 1024
 	//im,_:=gg.LoadImage("")
 	dc := gg.NewContext(S, S)
@@ -22,19 +32,85 @@ func main() {
 	dc.SavePNG("out.png")
 }
 
-func BlankImage(width,height int, c color.Color){
-	upLeft:= image.Point{}
-	lowRight:= image.Point{X: width, Y: height}
-	img:=image.NewRGBA(image.Rectangle{
+func readStyle() {
+	f, _ := os.Open("Certificate List - final.csv")
+	r := csv.NewReader(f)
+	d, _ := r.ReadAll()
+	fmt.Println(d[0])
+}
+
+func readData() {
+	f, err := os.Open("Certificate List - final.csv")
+	if err != nil {
+		panic(err)
+	}
+	r := csv.NewReader(f)
+	d, _ := r.ReadAll()
+	sCount, k := -1, -1
+	w, _ := os.Create("test.csv")
+	defer w.Close()
+	n := len(d[0])
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		if d[0][i][0] == '*' {
+			d[0][i] = d[0][i][1:]
+			sCount = i
+		} else if d[0][i][0] == '?' {
+			d[0][i] = d[0][i][1:]
+			k = i
+		}
+		printCSV(&i, &n, w, &d[0][i])
+	}
+	st, sm := "", make(map[string]int)
+	if len(d) < 2 {
+		panic("too small like something something")
+	}
+	for y := 1; y < len(d); y++ {
+		if k != -1 && len(d[y][k]) > 0 {
+			for x := 0; x < n; x++ {
+				var at *string
+				at = &d[y][x]
+				c := len(*at)
+				if x == sCount {
+					if c > 0 {
+						st = *at
+					}
+					sm[st]++
+					str := fmt.Sprintf(st, sm[st])
+					printCSV(&x, &n, w, &str)
+					continue
+				} else if c == 0 && y > 1 {
+					at = &d[arr[x]][x]
+				} else {
+					arr[x] = y
+				}
+				printCSV(&x, &n, w, at)
+			}
+		}
+	}
+}
+
+func printCSV(i, size *int, w *os.File, str *string) {
+	if *i+1 == *size {
+		w.WriteString(*str + "\n")
+	} else {
+		w.WriteString(*str + ",")
+	}
+}
+
+func BlankImage(width, height int, c color.Color) {
+	upLeft := image.Point{}
+	lowRight := image.Point{X: width, Y: height}
+	img := image.NewRGBA(image.Rectangle{
 		Min: upLeft,
 		Max: lowRight,
 	})
 	for y := 0; y < lowRight.Y; y++ {
 		for x := 0; x < lowRight.X; x++ {
-			img.Set(x,y,c)
+			img.Set(x, y, c)
 		}
 	}
-	f,_:=os.Create("gen.png")
+	f, _ := os.Create("gen.png")
 	err := png.Encode(f, img)
 	if err != nil {
 		return
